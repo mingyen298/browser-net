40e9627b42ab1c66b2d818147f5ef98835cd6ca5
yen <oe327188@gmail.com>
2024-04-23 14:33:08 +0800

    first commit

---
+++ webpack/paths.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path')
+
+const PATHS = {
+  src: path.resolve(__dirname, '../src'),
+  build: path.resolve(__dirname, '../build'),
+};
+
+
+
+module.exports = {PATHS}
