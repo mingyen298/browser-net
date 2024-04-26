d21eeaf140c9faab0777a5d81ba6e1f41b0cc64a
yen <oe327188@gmail.com>
2024-04-26 23:14:41 +0800

    export utils and update version id

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as client from "./client/client";
 import * as router from "./router/router";
 import * as net from "./net/net";
 import * as packet from "./packet/packet";
+import * as utils from './utils'
 
-
-export {client,router,net,packet}
+export { client, router, net, packet, utils }
